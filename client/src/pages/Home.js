acbe2cd63984604918296e74b2e8a39090222d5b
Z. R <zmirza7701@gmail.com>
2023-03-02 19:25:15 -0600

    added pages

---
+++ client/src/pages/Home.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useQuery } from '@apollo/client';
+
+import NoteList from '../components/NoteList';
+import NoteForm from '../components/NoteForm';
+
+import { QUERY_NOTES } from '../utils/queries';
+
+const Home = () => {
+  const { loading, data } = useQuery(QUERY_NOTES);
+  const notes = data?.notes || [];
+
+  return (
+    <main>
+      <div className="flex-row justify-center">
+        <div
+          className="col-12 col-md-10 mb-3 p-3"
+          style={{ border: '1px dotted #1a1a1a' }}
+        >
+          <NoteForm />
+        </div>
+        <div className="col-12 col-md-8 mb-3">
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            <NoteList
+              notes={notes}
+              title="Note(s)..."
+            />
+          )}
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
