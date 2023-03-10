d2e984aed3503fbc35849d228b9d22f7dcd3c4f9
Chuca <Chuca78@users.noreply.github.com>
2023-03-09 20:18:01 -0500

    polished header

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -15,7 +15,7 @@ const Header = () => {
           <Link className="text-light" to="/">
             <h1 className="m-0">Notes</h1>
           </Link>
-          <p className="m-0">Get into the mind of a programmer.</p>
+          {/* <p className="m-0">Get into the mind of a programmer.</p> */}
         </div>
         <div>
           {Auth.loggedIn() ? (
